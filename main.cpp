0a57872a3105ef1dd3043e9d63bf611c0a7d5945
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-13 11:37:57 +0700

    Menambahkan Prosedur Input

--- main.cpp
+++ main.cpp
@@ -5,6 +5,28 @@ using namespace std;
 int  arr{20};  //Deklarasi variable global array dengan panjang 20
 int n; // Deklarasi variable global n untuk menyimpan elemen pada array
 
-int main(){
+void input(){ // Prosedur input
+    int d;
+    while (true){
+        if (n <=20){
+        
+        else {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+}
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i=0;i<n;i++){
+        cout << "Data ke-" << (i+i) << " = "
+        cin >> arr{i};
+    }
     
+}
+
+int main(){
+
 }
