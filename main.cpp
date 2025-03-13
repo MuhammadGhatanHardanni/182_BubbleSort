a45e354c831c4400453983cd406750dbf6c13ffc
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-13 11:33:57 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int  arr{20};  //Deklarasi variable global array dengan panjang 20
+int n; // Deklarasi variable global n untuk menyimpan elemen pada array
+
+int main(){
+    
+}
